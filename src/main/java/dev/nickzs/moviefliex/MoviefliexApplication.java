3d356995c0685395f8bb0d03f5f3f828d908e801
Níckolas Davi <nickolasdavi407@gmail.com>
2025-04-08 19:24:20 -0300

    feat: initialize project

---
+++ src/main/java/dev/nickzs/moviefliex/MoviefliexApplication.java
@@ -0,0 +1,25 @@
+package dev.nickzs.moviefliex;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviefliexApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory(".")
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+
+		SpringApplication.run(MoviefliexApplication.class, args);
+	}
+
+}
