f907048dd78047e06027615bace06b6c7d345d9a
Níckolas Davi <nickolasdavi407@gmail.com>
2025-07-07 16:45:27 -0300

    fix: mensagem personalizada para mais tipos de erro

--- src/main/java/dev/nickzs/moviefliex/config/ApplicationControllerAdvice.java
+++ src/main/java/dev/nickzs/moviefliex/config/ApplicationControllerAdvice.java
@@ -1,9 +1,14 @@
 package dev.nickzs.moviefliex.config;
 import dev.nickzs.moviefliex.exception.UserNameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -12,5 +17,19 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNotFound(UserNameOrPasswordInvalidException exception){
         return exception.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValid(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 }
